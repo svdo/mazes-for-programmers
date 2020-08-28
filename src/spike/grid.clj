91d1f3026708694ba592ff12dbe3bb3843c3a84b
Stefan van den Oord <soord@mac.com>
2020-08-28 20:19:48 +0200

    Grids can have linked cells

--- src/spike/grid.clj
+++ src/spike/grid.clj
@@ -2,7 +2,9 @@
 
 (defn create
   [width height]
-  (repeat height (repeat width [])))
+  (vec (for [row (range height)]
+         (vec (for [col (range width)]
+                {:row row :col col :links #{}})))))
 
 (defn height
   [grid]
@@ -16,4 +18,12 @@
   [grid row col]
   (when (and (>= (dec (height grid)) row 0)
              (>= (dec (width grid)) col 0))
-    {}))
+    (get-in grid [row col])))
+
+(defn link-cells
+  [grid [row1 col1] [row2 col2]]
+  (-> grid
+      (update-in [row1 col1 :links]
+                 #(conj % (cell grid row2 col2)))
+      (update-in [row2 col2 :links]
+                 #(conj % (cell grid row1 col1)))))
--- test/spike/grid_test.clj
+++ test/spike/grid_test.clj
@@ -2,15 +2,34 @@
   (:require [clojure.test :refer [deftest testing is]]
             [spike.grid :as grid]))
 
+(def grid-10-10 (grid/create 10 10))
+
 (deftest grid-test
   (testing "create a new grid"
-    (is (grid/create 10 10))
     (is (= 20 (-> (grid/create 30 20) grid/height)))
     (is (= 30 (-> (grid/create 30 20) grid/width))))
 
   (testing "getting cells"
-    (is (not (nil? (-> (grid/create 10 10) (grid/cell 0 0)))))
-    (is (nil? (-> (grid/create 10 10) (grid/cell -1 0))))
-    (is (nil? (-> (grid/create 10 10) (grid/cell 0 -1))))
-    (is (nil? (-> (grid/create 10 10) (grid/cell 10 0))))
-    (is (nil? (-> (grid/create 10 10) (grid/cell 0 10))))))
+    (is (not (nil? (-> grid-10-10 (grid/cell 0 0)))))
+    (is (nil? (-> grid-10-10 (grid/cell -1 0))))
+    (is (nil? (-> grid-10-10 (grid/cell 0 -1))))
+    (is (nil? (-> grid-10-10 (grid/cell 10 0))))
+    (is (nil? (-> grid-10-10 (grid/cell 0 10)))))
+
+  (testing "cells know their location"
+    (is (= 2 (:row (-> grid-10-10 (grid/cell 2 3)))))
+    (is (= 3 (:col (-> grid-10-10 (grid/cell 2 3))))))
+
+  (testing "initially no links"
+    (is (empty? (:links (-> grid-10-10 (grid/cell 1 1))))))
+
+  (testing "creating a link is bidirectional"
+    (is (let [updated-grid (grid/link-cells grid-10-10 [1 1] [1 2])]
+          (and (= #{(grid/cell grid-10-10 1 2)}
+                  (-> updated-grid
+                      (grid/cell 1 1)
+                      :links))
+               (= #{(grid/cell grid-10-10 1 1)}
+                  (-> updated-grid
+                      (grid/cell 1 2)
+                      :links)))))))
