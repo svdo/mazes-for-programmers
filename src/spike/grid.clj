d538ca6db74dbef7bafda770532b63b70f185083
Stefan van den Oord <soord@mac.com>
2020-08-28 17:11:08 +0200

    We have cells

--- src/spike/grid.clj
+++ src/spike/grid.clj
@@ -11,3 +11,9 @@
 (defn width
   [grid]
   (count (first grid)))
+
+(defn cell
+  [grid row col]
+  (when (and (>= (dec (height grid)) row 0)
+             (>= (dec (width grid)) col 0))
+    {}))
--- test/spike/grid_test.clj
+++ test/spike/grid_test.clj
@@ -6,4 +6,11 @@
   (testing "create a new grid"
     (is (grid/create 10 10))
     (is (= 20 (-> (grid/create 30 20) grid/height)))
-    (is (= 30 (-> (grid/create 30 20) grid/width)))))
+    (is (= 30 (-> (grid/create 30 20) grid/width))))
+
+  (testing "getting cells"
+    (is (not (nil? (-> (grid/create 10 10) (grid/cell 0 0)))))
+    (is (nil? (-> (grid/create 10 10) (grid/cell -1 0))))
+    (is (nil? (-> (grid/create 10 10) (grid/cell 0 -1))))
+    (is (nil? (-> (grid/create 10 10) (grid/cell 10 0))))
+    (is (nil? (-> (grid/create 10 10) (grid/cell 0 10))))))
