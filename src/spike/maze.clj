62040d0b0c0dd11290904fc5aae7c304d0d48427
Stefan van den Oord <soord@mac.com>
2020-08-30 20:01:05 +0200

    Use lanterna for showing the maze

--- src/spike/maze.clj
+++ src/spike/maze.clj
@@ -4,13 +4,17 @@
             #_[spike.generate.binary-tree :as binary-tree]
             [spike.generate.sidewinder :as sidewinder]
             #_[spike.render.ascii :as ascii]
-            [spike.render.box :as box]))
+            [spike.render.box :as box]
+            [lanterna.terminal :as t]))
 
 (defn -main
   [size-str & _]
-  (let [size (read-string size-str)]
-    (-> (grid/create size size)
-        ;; binary-tree/carve
-        sidewinder/carve
-        box/to-str
-        println)))
+  (let [size (read-string size-str)
+        term (t/get-terminal :unix)]
+    (t/start term)
+    (t/put-string term
+                  (-> (grid/create size size)
+                      ;; binary-tree/carve
+                      sidewinder/carve
+                      box/to-str))
+    (read-line)))
