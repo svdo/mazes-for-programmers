b8479a00b3ed817c2851d4a3c83b16e91e9b463a
Stefan van den Oord <soord@mac.com>
2021-11-24 17:39:37 +0100

    Render using react components (so that it properly updates when grid changes)

--- src/spike/render/helix.cljs
+++ src/spike/render/helix.cljs
@@ -1,5 +1,6 @@
 (ns spike.render.helix
   (:require [spike.grid :as grid]
+            [helix.core :refer [$ <>]]
             [helix.dom :as d]
             [maze.lib :refer [defnc]]
             [clojure.string :as str]))
@@ -14,46 +15,49 @@
         element "1fr "]
     (apply str border (repeat num-cells (str element border)))))
 
-(defn- h-border [grid w h row]
+(defnc BorderPart [{:keys [class-name]}]
+  (d/div {:class-name class-name}))
+
+(defnc h-border [{:keys [grid w h row]}]
   (let [class-name-base (cond-> "border horizontal"
                           (= row -1)
                           (str " first")
 
                           (= row (dec h))
                           (str " last"))]
-    (cons (d/div {:class-name class-name-base
-                  :key (str "b-" (* 2 row) "-" -1)})
-          (mapcat (fn [col]
-                    [(let [cell (grid/get-cell grid row col)
-                           class-name (cond-> class-name-base
-                                        (= w col)
-                                        (str " last")
+    (<> (cons ($ BorderPart {:class-name class-name-base
+                             :key (str "b-" (* 2 row) "-" -1)})
+              (mapcat (fn [col]
+                        [(let [cell (grid/get-cell grid row col)
+                               class-name (cond-> class-name-base
+                                            (= w col)
+                                            (str " last")
 
-                                        (grid/linked? cell :south)
-                                        (str " open"))]
-                       (d/div {:class-name class-name
-                               :key (str "b-" (* 2 row) "-" (* 2 col))}))
-                     (d/div {:class-name class-name-base
-                             :key (str "b-" (* 2 row) "-" (inc (* 2 col)))})])
-                  (range 0 w)))))
+                                            (grid/linked? cell :south)
+                                            (str " open"))]
+                           ($ BorderPart {:class-name class-name
+                                          :key (str "b-" (* 2 row) "-" (* 2 col))}))
+                         ($ BorderPart {:class-name class-name-base
+                                        :key (str "b-" (* 2 row) "-" (inc (* 2 col)))})])
+                      (range 0 w))))))
 
-(defn- cells-row [grid content-fn w row]
-  (cons (d/div {:class-name "border vertical first"
-                :key (str "b-" (inc (* 2 row)) "-" -1)})
-        (mapcat (fn [col]
-                  (let [cell (grid/get-cell grid row col)]
-                    [(d/div {:class-name "cell"
-                             :key (str "c-" (inc (* 2 row)) "-" (* 2 col))}
-                            (content-fn cell))
-                     (let [class-name (cond-> "border vertical"
-                                        (= (dec w) col)
-                                        (str " last")
-                                        
-                                        (grid/linked? cell :east)
-                                        (str " open"))]
-                       (d/div {:class-name class-name
-                               :key (str "b-" (inc (* 2 row)) "-" (inc (* 2 col)))}))]))
-                (range 0 w))))
+(defnc cells-row [{:keys [grid content-fn w row]}]
+  (<> (cons (d/div {:class-name "border vertical first"
+                    :key (str "b-" (inc (* 2 row)) "-" -1)})
+            (mapcat (fn [col]
+                      (let [cell (grid/get-cell grid row col)]
+                        [(d/div {:class-name "cell"
+                                 :key (str "c-" (inc (* 2 row)) "-" (* 2 col))}
+                                (content-fn cell))
+                         (let [class-name (cond-> "border vertical"
+                                            (= (dec w) col)
+                                            (str " last")
+
+                                            (grid/linked? cell :east)
+                                            (str " open"))]
+                           (d/div {:class-name class-name
+                                   :key (str "b-" (inc (* 2 row)) "-" (inc (* 2 col)))}))]))
+                    (range 0 w)))))
 
 (defnc Grid [{grid :grid
               content-fn :content-fn :or {content-fn (constantly " ")}}]
@@ -65,8 +69,8 @@
               :grid-template-columns (grid-template-spec w)
               :width (* 50 w)
               :height (* 50 h)}}
-     (h-border grid w h -1)
-     (mapcat (fn [row]
-               (concat (cells-row grid content-fn w row)
-                       (h-border grid w h row)))
-             (range 0 h)))))
+     ($ h-border {:grid grid :w w :h h :row -1})
+     (<> (mapcat (fn [row]
+                   [($ cells-row {:key (str "row-" row) :grid grid :content-fn content-fn :w w :row row})
+                    ($ h-border {:key (str "h-border-" row) :grid grid :w w :h h :row row})])
+                 (range 0 h))))))
