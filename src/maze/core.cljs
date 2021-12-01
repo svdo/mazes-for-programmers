cc1bf5dff87e5730ae9aa7eab490f60158cbc5c6
Stefan van den Oord <soord@mac.com>
2021-12-01 09:20:35 +0100

    Allow toggling colors

--- src/maze/core.cljs
+++ src/maze/core.cljs
@@ -11,20 +11,24 @@
 
 (defnc Maze [{:keys [grid]}]
   (let [[starting-point set-starting-point] (hooks/use-state [0 0])
+        [show-colors set-show-colors] (hooks/use-state false)
         {:keys [from to distances]} (-> grid
                                         (dijkstra/find-longest-path starting-point))
         grid (dijkstra/mark-shortest-path distances from to)
         max-distance (apply max (flatten (grid/map-cells :dijkstra/distance grid)))]
-    ($ Grid {:grid grid
-             :set-starting-point set-starting-point
-             :content-fn
-             #_(comp str :dijkstra/distance)
-             #(if (:dijkstra/on-shortest-path %) (:dijkstra/distance %) " ")
-             :color-fn #(let [distance (:dijkstra/distance %)
-                              intensity (- 1.0 (/ distance max-distance))
-                              dark (* 255.0 intensity)
-                              bright (+ 128.0 (* 127 intensity))]
-                          {:red dark :green bright :blue dark :alpha 0.5})})))
+    (<>
+     (d/button {:on-click #(set-show-colors (not show-colors))} "Toggle colors")
+     ($ Grid {:grid grid
+              :set-starting-point set-starting-point
+              :content-fn
+              #_(comp str :dijkstra/distance)
+              #(if (:dijkstra/on-shortest-path %) (:dijkstra/distance %) " ")
+              :color-fn (when show-colors
+                          #(let [distance (:dijkstra/distance %)
+                                 intensity (- 1.0 (/ distance max-distance))
+                                 dark (* 255.0 intensity)
+                                 bright (+ 128.0 (* 127 intensity))]
+                             {:red dark :green bright :blue dark :alpha 0.5}))}))))
 
 (defnc App []
   (let [[size set-size] (hooks/use-state 10)
